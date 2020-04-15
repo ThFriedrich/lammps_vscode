c72c92beeb32c94fe220b76da913565435b22be3
arn-all <arnaud.allera@univ-lyon1.fr>
2020-04-15 13:36:06 +0200

    Added hyperlink to hover and autocompletion #2

--- src/get_doc.ts
+++ src/get_doc.ts
@@ -5,17 +5,27 @@ export function get_doc(find_command: string) {
     return command_docs.find(e => e.command === find_command);
 }
 
-export function get_completion_list() {
+export function get_completion_list(CompletionString: string, detail: boolean) {
 
     const completion_List = new CompletionList();
 
     for (let c of command_docs.values()) {
         var compl_it = new CompletionItem(c.command);
         compl_it.documentation = new MarkdownString();
-        compl_it.documentation.appendCodeblock(c.syntax)
+        if (detail) {
+            compl_it.detail = c.syntax
+        } 
+        else {
+            compl_it.documentation.appendCodeblock(c.syntax, 'lmps')
+        }
+        compl_it.documentation.appendMarkdown("[Open documentation](https://lammps.sandia.gov/doc/" + c.html_filename+")\n")
         compl_it.documentation.appendMarkdown(c.parameters)
         compl_it.documentation.appendMarkdown(" \n" + "--- " +" \n")
-        compl_it.documentation.appendText(c.description)
+        compl_it.documentation.appendText(c.short_description)
+        
+        if (CompletionString == 'Syntax') {
+            compl_it.insertText = c.syntax
+        }
         completion_List.items.push(compl_it)
     }
     return completion_List
