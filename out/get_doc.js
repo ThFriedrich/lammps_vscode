01bfbf2a0617bf43ef4388659d0c0d8917e1a833
thfriedrich <thomas.friedrich87@outlook.de>
2020-04-18 12:58:41 +0200

    Support multiple commands per documentation array-element

--- out/get_doc.js
+++ out/get_doc.js
@@ -3,28 +3,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const lmp_doc_1 = require("./lmp_doc");
 const vscode_1 = require("vscode");
 function get_doc(find_command) {
-    return lmp_doc_1.command_docs.find(e => e.command === find_command);
+    return lmp_doc_1.command_docs.find(e => e.command.includes(find_command));
 }
 exports.get_doc = get_doc;
 function get_completion_list(CompletionString, detail) {
     const completion_List = new vscode_1.CompletionList();
     for (let c of lmp_doc_1.command_docs.values()) {
-        var compl_it = new vscode_1.CompletionItem(c.command);
-        compl_it.documentation = new vscode_1.MarkdownString();
-        if (detail) {
-            compl_it.detail = c.syntax;
+        for (let c_ix of c.command.values()) {
+            var compl_it = new vscode_1.CompletionItem(c_ix);
+            compl_it.documentation = new vscode_1.MarkdownString();
+            if (detail) {
+                compl_it.detail = c.syntax;
+            }
+            else {
+                compl_it.documentation.appendCodeblock(c.syntax, 'lmps');
+            }
+            compl_it.documentation.appendMarkdown("[Open documentation](https://lammps.sandia.gov/doc/" + c.html_filename + ")\n");
+            compl_it.documentation.appendMarkdown(c.parameters);
+            compl_it.documentation.appendMarkdown(" \n" + "--- " + " \n");
+            compl_it.documentation.appendText(c.short_description);
+            if (CompletionString == 'Syntax') {
+                compl_it.insertText = c.syntax;
+            }
+            completion_List.items.push(compl_it);
         }
-        else {
-            compl_it.documentation.appendCodeblock(c.syntax, 'lmps');
-        }
-        compl_it.documentation.appendMarkdown("[Open documentation](https://lammps.sandia.gov/doc/" + c.html_filename + ")\n");
-        compl_it.documentation.appendMarkdown(c.parameters);
-        compl_it.documentation.appendMarkdown(" \n" + "--- " + " \n");
-        compl_it.documentation.appendText(c.short_description);
-        if (CompletionString == 'Syntax') {
-            compl_it.insertText = c.syntax;
-        }
-        completion_List.items.push(compl_it);
     }
     return completion_List;
 }
