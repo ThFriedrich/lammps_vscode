d713821aff87a1d66f79f832e12e221ff2f98be0
Thomas <thomas.friedrich87@outlook.de>
2023-09-18 18:02:54 +0200

    Add dry-run #11 and args for task configs #41

--- src/task_fcns.ts
+++ src/task_fcns.ts
@@ -4,9 +4,9 @@ const type = "lmps";
 
 interface lmp_task extends TaskDefinition {
     binary: string;
-    task: string;
     mpi_tasks?: Number;
     gpu_nodes?: Number;
+    args?: string
 }
 
 export function get_tasks(): Task[] {
@@ -16,6 +16,7 @@ export function get_tasks(): Task[] {
     const execution_mpi = new ShellExecution(`mpirun -np ${lmp_tsk.mpi_tasks.toString()} ${lmp_tsk.binary} -in ${file}`);
     const execution_gpu = new ShellExecution(`${lmp_tsk.binary} -sf gpu -pk gpu ${lmp_tsk.gpu_nodes.toString()} -in ${file}`);
     const execution_mpi_gpu = new ShellExecution(`mpirun -np ${lmp_tsk.mpi_tasks.toString()} ${lmp_tsk.binary} -sf gpu -pk gpu ${lmp_tsk.gpu_nodes.toString()} -in ${file}`);
+    const execution_skip = new ShellExecution(`${lmp_tsk.binary} -in ${file} -skiprun`);
     return [
         new Task({ type: type }, TaskScope.Workspace,
             "Run Single-Task", "lmps", execution_cpu, []),
@@ -24,7 +25,9 @@ export function get_tasks(): Task[] {
         new Task({ type: type }, TaskScope.Workspace,
             "Run Single-Task GPU", "lmps", execution_gpu, []),
         new Task({ type: type }, TaskScope.Workspace,
-            "Run Multi-Task GPU", "lmps", execution_mpi_gpu, [])
+            "Run Multi-Task GPU", "lmps", execution_mpi_gpu, []),
+        new Task({ type: type }, TaskScope.Workspace,
+            "Run Single-Task (Dry-run)", "lmps", execution_skip, []),
     ];
 }
 
@@ -32,35 +35,22 @@ export function resolve_task(tsk: Task): Task | undefined {
     const file = window.activeTextEditor?.document.fileName
     const tdf: lmp_task = <any>tsk.definition;
     const execution = new ShellExecution('')
-    switch (tdf.task) {
-        case "Run Single-Task":
-            execution.commandLine = `${tdf.binary} -in ${file}`;
-            break;
-        case "Run Multi-Task":
-            if (tdf.mpi_tasks) {
-                execution.commandLine = `mpirun -np ${tdf.mpi_tasks.toString()} ${tdf.binary} -in ${file}`;
-            }
-            break;
-        case "Run Single-Task GPU":
-            if (tdf.gpu_nodes) {
-                execution.commandLine = `${tdf.binary} -sf gpu -pk gpu ${tdf.gpu_nodes.toString()} -in ${file}`;
-            }
-            break;
-        case "Run Multi-Task GPU":
-            if (tdf.mpi_tasks && tdf.gpu_nodes) {
-                execution.commandLine = `mpirun -np ${tdf.mpi_tasks.toString()} ${tdf.binary} -sf gpu -pk gpu ${tdf.gpu_nodes.toString()} -in ${file}`;
-            }
-            break;
-        default:
-            return undefined
+    if (tdf.mpi_tasks && tdf.gpu_nodes) { // Multi-Task GPU
+        execution.commandLine = `mpirun -np ${tdf.mpi_tasks.toString()} ${tdf.binary} -sf gpu -pk gpu ${tdf.gpu_nodes.toString()} -in ${file} ${tdf.args}`;
     }
-    const wsf: WorkspaceFolder | undefined = workspace.getWorkspaceFolder(window.activeTextEditor!.document?.uri)
-    if (wsf) {
-        return new Task(
-            tdf,
-            wsf,
-            tdf.task,
-            'lmps',
-            execution);
+    else if (!tdf.mpi_tasks && tdf.gpu_nodes) { // Single-Task GPU
+        execution.commandLine = `${tdf.binary} -sf gpu -pk gpu ${tdf.gpu_nodes.toString()} -in ${file} ${tdf.args}`;
     }
+    else if (tdf.mpi_tasks && !tdf.gpu_nodes) { // Multi-Task CPU
+        execution.commandLine = `mpirun -np ${tdf.mpi_tasks.toString()} ${tdf.binary} -in ${file} ${tdf.args}`;
+    }
+    else { // Single-Task CPU
+        execution.commandLine = `${tdf.binary} -in ${file} ${tdf.args}`;
+    }
+    return new Task(
+        tdf,
+        TaskScope.Workspace,
+        "Run",
+        "lmps",
+        execution);
 }
